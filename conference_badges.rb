a51b230feba2ac1977cf32c9370c4998462d586e
Chuck Smith <chuck@eclecticsaddlebag.com>
2019-09-09 23:56:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,11 +4,15 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(attendees)
-  attendees.each do |name|
-    puts "Hello, my name is #{name}."
+def batch_badge_creator(array)
+  new_array = []
+  array.each do |name|
+    new_array.push("Hello, my name is #{name}.")
   end
+  return new_array
 end
+
+# batch_badge_creator(name)
   
 
 
