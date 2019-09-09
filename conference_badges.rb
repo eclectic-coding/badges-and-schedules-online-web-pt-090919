a574c21bcb6f0b85db1b877c9379e9e9124f0733
Chuck Smith <chuck@eclecticsaddlebag.com>
2019-09-09 23:51:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,10 +1,14 @@
 attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
-def badge_maker(attendees)
-  attendees.each do |name|
-  puts "Hello, my name is #{name}."
-  end
+def badge_maker(name)
+  return "Hello, my name is #{name}."
 end
+
+# def badge_maker(attendees)
+#   attendees.each do |name|
+#   puts "Hello, my name is #{name}."
+#   end
+# end
 
 # badge_maker(attendees)
   
