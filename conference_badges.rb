4fcede5207207ab161b12616f2f2394865f924c7
Chuck Smith <chuck@eclecticsaddlebag.com>
2019-09-10 00:00:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,3 +12,12 @@ def batch_badge_creator(array)
   return new_array
 end
 
+def assign_rooms(array)
+  new_array = []
+  counter = 1 
+  array.each do |name|
+    new_array.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+    counter += 1 
+  end
+  return new_array
+end
